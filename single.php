4a4d98738b9d0384944575cc39f6d05caea75e63
reeganaga <rega.blank@gmail.com>
2016-10-05 15:03:54 +0700

    add single team member

--- single.php
+++ single.php
@@ -5,29 +5,11 @@
 			<!-- display title post -->
 			<h2><a href="<?php the_permalink(); ?>" title="Permentn link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 			<!-- display author  -->
-			<?php if (!empty(get_post_meta(get_the_ID(),'teamManagerPosition',true)) ): ?>
-
-				<?php
-				// var_dump(get_post_meta(get_the_ID()));  
-				$img_id = get_post_meta(get_the_ID(),'teamManagerImage',true);
-				$image =  wp_get_attachment_image_src($img_id); ?>
-				<div class="row" style="margin-bottom: 20px;">
-					<div class="col-md-4 col-md-offset-4">
-						<img src="<?php echo $image[0]; ?>" class="img-responsive img-circle">
-					</div>
-				</div>
-				<ul class="list-group">
-				  <li class="list-group-item">Position : <b><?php echo get_post_meta(get_the_ID(),'teamManagerPosition',true) ?></b></li>
-				  <li class="list-group-item">Email : <b><?php echo get_post_meta(get_the_ID(),'teamManagerEmail',true) ?></b></li>
-				  <li class="list-group-item">Website : <b><?php echo get_post_meta(get_the_ID(),'teamManagerWebsite',true); ?></b></li>
-				</ul>
-			<?php $position = get_post_meta(get_the_ID(),'teamManagerPosition',true); ; ?>
-			<?php endif ?>
 
 			<small><?php the_time('F jS, Y'); ?> by <?php the_author_posts_link(); ?></small>
 			<div class="text-justify">
 				<?php the_content(); ?>
-				<!-- <p class="postmetadata"><?php _e('Posted in'); ?> <?php the_category(', '); ?></p> -->
+				<p class="postmetadata"><?php _e('Posted in'); ?> <?php the_category(', '); ?></p>
 			</div>
 		<?php endwhile; else : ?>	
 			<p><?php _e('Sorry, no post marched your criteria.'); ?></p>
